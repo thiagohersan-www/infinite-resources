c7faa822536d5e7f78c31d9ddba6dc558dc26fa3
Thiago Hersan <thiago.hersan+github@gmail.com>
2021-10-28 23:13:23 -0400

    isolates gui logic

---
+++ js/Gui.js
@@ -0,0 +1,42 @@
+import { Scroll } from './Scroll.js';
+import { Strip } from './Strip.js';
+import { clearScene } from './clear.js';
+
+class Gui {
+  constructor(scene) {
+    this.scene = scene;
+
+    document.getElementById('param-num-layer').value = 8;
+    document.getElementById('param-amp').value = 1;
+    document.getElementById('param-x-scale').value = 160;
+    document.getElementById('param-y-scale').value = 10;
+
+    Array.from(document.getElementsByTagName('input')).forEach(el => {
+      el.addEventListener('change', this.updateParam);
+      el.parentElement.getElementsByClassName('param-value')[0].innerHTML = el.value;
+    });
+  }
+
+  updateParam = (ev) => {
+    const el = ev.target;
+    const elId = el.id;
+
+    el.parentElement.getElementsByClassName('param-value')[0].innerHTML = el.value;
+
+    clearScene(this.scene);
+
+    if (elId.includes('-amp')) {
+      Strip.AMPLITUDE = 1.000005 * el.value;
+    } else if (elId.includes('-num-layer')) {
+      Scroll.NSTRIPS_ONSCREEN = el.value;
+      Scroll.STRIP_HEIGHT = window.innerHeight / Scroll.NSTRIPS_ONSCREEN;
+    } else if (elId.includes('-x-scale')) {
+      Strip.DIVERSITY_X = el.value;
+    } else if (elId.includes('-y-scale')) {
+      Strip.DIVERSITY_Y = el.value;
+    }
+    new Scroll(this.scene);
+  }
+}
+
+export { Gui }
--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 import * as THREE from './three/three.module.js';
+import { Gui } from './Gui.js';
 import { Scroll } from './Scroll.js';
-import { Strip } from './Strip.js';
-import { clearScene } from './clear.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(150, window.innerWidth / window.innerHeight, 1, 200);
@@ -34,39 +33,10 @@ window.addEventListener('DOMContentLoaded', () => {
   mScrollDiv.style.height = `${15 * window.innerHeight}px`;
   setTimeout(centerScroll, 500);
 
-  // GUI
-  setParams();
-  Array.from(document.getElementsByTagName('input')).forEach(el => {
-    el.addEventListener('change', updateParams);
-  });
+  new Scroll(scene);
+  new Gui(scene);
 });
 
-function setParams() {
-  document.getElementById('param-num-layer').value = 8;
-  document.getElementById('param-amp').value = 1;
-  document.getElementById('param-x-scale').value = 160;
-  document.getElementById('param-y-scale').value = 10;
-}
-
-function updateParams(ev) {
-  const inputId = ev.target.id;
-
-  clearScene(scene);
-
-  if (inputId.includes('-amp')) {
-    Strip.AMPLITUDE = 1.000005 * ev.target.value;
-  } else if (inputId.includes('-num-layer')) {
-    Scroll.NSTRIPS_ONSCREEN = ev.target.value;
-    Scroll.STRIP_HEIGHT = window.innerHeight / Scroll.NSTRIPS_ONSCREEN;
-  } else if (inputId.includes('-x-scale')) {
-    Strip.DIVERSITY_X = ev.target.value;
-  } else if (inputId.includes('-y-scale')) {
-    Strip.DIVERSITY_Y = ev.target.value;
-  }
-  new Scroll(scene);
-}
-
-const mScroll = new Scroll(scene);
 const MAX_DEPTH = -(Scroll.NSTRIPS_TOTAL - 1) * Scroll.STRIP_HEIGHT;
 
 let previousScrollTop = 0;
