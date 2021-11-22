daf8d3eba4058a7c00b4542d1c494b852a57dacc
Thiago Hersan <thiago.hersan+github@gmail.com>
2021-11-21 19:31:03 -0500

    refactors horizontal/vertical, high/low res logic

--- js/Strip.js
+++ js/Strip.js
@@ -9,17 +9,17 @@ const BY_COLOR = BY_RGB.concat(BY_HLS);
 
 class Strip {
   static getTopMesh(width, stripHeight, render) {
-    const isDesktop = (width > Strip.MOBILE_WIDTH) && (width > window.innerHeight);
+    const isHorizontal = (window.innerWidth > window.innerHeight);
 
     const mLoader = new THREE.TextureLoader();
-    const tFilename = `./assets/map00-${isDesktop ? 'desktop' : 'mobile'}.jpg`;
+    const tFilename = `./assets/map00-${isHorizontal ? 'horizontal' : 'vertical'}.jpg`;
 
     mLoader.load(tFilename, (texture) => {
       const imageHeight = width * texture.image.height / texture.image.width;
 
       texture.repeat.set(1 / width, 1 / imageHeight);
 
-      const meshY = isDesktop ? (0.5 * Strip.AMPLITUDE * stripHeight) : (window.innerHeight - imageHeight);
+      const meshY = isHorizontal ? (0.5 * Strip.AMPLITUDE * stripHeight) : (window.innerHeight - imageHeight);
 
       mMesh.position.set(0, meshY, -0.005);
 
@@ -47,7 +47,8 @@ class Strip {
       return Strip.getTopMesh(width, height, render);
     }
 
-    const fullWidth = (width * window.devicePixelRatio > Strip.MOBILE_WIDTH);
+    const isFullWidth = (width * window.devicePixelRatio > Strip.MOBILE_WIDTH);
+    const isHorizontal = (window.innerWidth > window.innerHeight);
 
     yidx = yidx - 1;
     const mLoader = new THREE.TextureLoader();
@@ -80,23 +81,25 @@ class Strip {
     });
 
     const mShape = new THREE.Shape();
-    const deltaX = width / Strip.NUM_POINTS_X;
+    const numPointsX = isFullWidth ? Strip.NUM_POINTS_X : 0.5 * Strip.NUM_POINTS_X;
+    const diversityX = isHorizontal ? Strip.DIVERSITY_X : 0.5 * Strip.DIVERSITY_X;
+    const deltaX = width / numPointsX;
 
     mShape.moveTo(0, height);
-    for (let i = 0; i <= Strip.NUM_POINTS_X; i++) {
+    for (let i = 0; i <= numPointsX; i++) {
       const x = i * deltaX;
-      const y_noise = Strip.NOISE.noise2D(x / Strip.DIVERSITY_X, yidx / Strip.DIVERSITY_Y);
-      const y_noise_h = Strip.NOISE.noise2D(Strip.DIVERSITY_X_HIGH_FACTOR * x / Strip.DIVERSITY_X, yidx / Strip.DIVERSITY_Y);
+      const y_noise = Strip.NOISE.noise2D(x / diversityX, yidx / Strip.DIVERSITY_Y);
+      const y_noise_h = Strip.NOISE.noise2D(Strip.DIVERSITY_X_HIGH_FACTOR * x / diversityX, yidx / Strip.DIVERSITY_Y);
 
       const firstLayerDamp = (yidx === 0) ? 0.5 : 1.0;
       const y = firstLayerDamp * height * Strip.AMPLITUDE * (y_noise + Strip.DIVERSITY_X_HIGH_AMP * y_noise_h);
       mShape.lineTo(x, height + y - Strip.SPACER);
     }
 
-    for (let i = 0; i <= Strip.NUM_POINTS_X; i++) {
+    for (let i = 0; i <= numPointsX; i++) {
       const x = width - i * deltaX;
-      const y_noise = Strip.NOISE.noise2D(x / Strip.DIVERSITY_X, (yidx + 1) / Strip.DIVERSITY_Y);
-      const y_noise_h = Strip.NOISE.noise2D(Strip.DIVERSITY_X_HIGH_FACTOR * x / Strip.DIVERSITY_X, (yidx + 1) / Strip.DIVERSITY_Y);
+      const y_noise = Strip.NOISE.noise2D(x / diversityX, (yidx + 1) / Strip.DIVERSITY_Y);
+      const y_noise_h = Strip.NOISE.noise2D(Strip.DIVERSITY_X_HIGH_FACTOR * x / diversityX, (yidx + 1) / Strip.DIVERSITY_Y);
 
       const y = height * Strip.AMPLITUDE * (y_noise + Strip.DIVERSITY_X_HIGH_AMP * y_noise_h);
       mShape.lineTo(x, y + Strip.SPACER);
@@ -114,7 +117,7 @@ class Strip {
 // Strip.NOISE = new SimplexNoise(new Date());
 Strip.NOISE = new SimplexNoise('infinitum');
 
-Strip.MOBILE_WIDTH = 800;
+Strip.MOBILE_WIDTH = 1090;
 
 Strip.NUM_POINTS_X = 256.0;
 
@@ -122,7 +125,7 @@ Strip.NUM_POINTS_X = 256.0;
 Strip.AMPLITUDE = 0.7;
 
 // x-diversity: 200 - (160)
-Strip.DIVERSITY_X = (window.innerWidth > Strip.MOBILE_WIDTH) ? 180.0 : 90.0;
+Strip.DIVERSITY_X = 180.0;
 
 Strip.DIVERSITY_X_HIGH_FACTOR = 4.0;
 Strip.DIVERSITY_X_HIGH_AMP = 0.2;
