2eb553c4991ec560f90674f6168ddbcea59ef58f
Thiago Hersan <thiago.hersan+github@gmail.com>
2023-06-24 13:44:37 -0400

    implements overlay/shadow and info logic

---
+++ js/Overlay.js
@@ -0,0 +1,34 @@
+class Overlay {
+  constructor() {
+    document.getElementById('hide-overlay-button').addEventListener('click', Overlay.hideOverlay);
+    document.getElementById('my-popup').addEventListener('click', (e) => e.stopPropagation());
+    document.getElementById('my-overlay').addEventListener('click', Overlay.hideOverlay);
+    document.getElementById('my-info-button').addEventListener('click', Overlay.showOverlay);
+  }
+
+  static showOverlay() {
+    document.getElementById('my-overlay').style.display = 'flex';
+    setTimeout(() => document.getElementById('my-overlay').style.opacity = 1, 100);
+
+    // window.removeEventListener('wheel', onScrollDesktop);
+    // window.removeEventListener('touchmove', onScrollMobile);
+    window.addEventListener('keyup', Overlay.checkEscKey);
+  }
+
+  static hideOverlay() {
+    document.getElementById('my-overlay').style.opacity = 0;
+    setTimeout(() => document.getElementById('my-overlay').style.display = 'none', 200);
+
+    // window.addEventListener('wheel', onScrollDesktop, { passive: false });
+    // window.addEventListener('touchmove', onScrollMobile, { passive: false });
+    window.removeEventListener('keyup', Overlay.checkEscKey);
+  }
+
+  static checkEscKey = (event) => {
+    if (event.key && (event.key === 'Escape' || event.key === 'Esc')) {
+      Overlay.hideOverlay();
+    }
+  };
+}
+
+export { Overlay };
--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+import { Overlay } from './Overlay.js';
 import { Scene } from './Scene.js';
 import { Scroll } from './Scroll.js';
 import { Strip } from './Strip.js';
@@ -6,73 +7,47 @@ const urlParams = new URLSearchParams(window.location.search);
 const AUTO_SCROLL = urlParams.has('autoScroll');
 const AUTO_SCROLL_SPEED = parseFloat(urlParams.get('autoScroll')) || 1.0;
 
-const LAYERS_Y_OFFSET = -window.innerHeight / 2.0 - (Scroll.STRIP_HEIGHT + 0.5 * Strip.AMPLITUDE * Scroll.STRIP_HEIGHT);
+const LAYERS_Y_OFFSET = () => {
+  return window.innerHeight / 2.0 - (Scroll.STRIP_HEIGHT + 0.5 * Strip.AMPLITUDE * Scroll.STRIP_HEIGHT);
+};
 
+let currentLayersOffsetY = LAYERS_Y_OFFSET();
 let currentHeight = window.innerHeight;
+
 const setupScene = () => {
   if (window.innerHeight < currentHeight) return;
 
   currentHeight = window.innerHeight;
+  currentLayersOffsetY = LAYERS_Y_OFFSET();
   document.getElementById('my-shadow-div').style.height = `${window.innerHeight}px`;
-  // TODO:
-  // camera.aspect = window.innerWidth / window.innerHeight;
-  // if (window.mScroll === undefined) {
-  //   scene.position.setY(LAYERS_Y_OFFSET);
-  // }
+  document.getElementById("my-layer-container").style.paddingTop = `${currentLayersOffsetY}px`;
 };
-
 window.addEventListener('resize', setupScene);
 
 window.addEventListener('DOMContentLoaded', () => {
   setupScene();
 
-  // INFO
-  document.getElementById('hide-overlay-button').addEventListener('click', hideOverlay);
-  document.getElementById('my-popup').addEventListener('click', (e) => e.stopPropagation());
-  document.getElementById('my-overlay').addEventListener('click', hideOverlay);
-  document.getElementById('my-info-button').addEventListener('click', showOverlay);
-
+  window.mOverlay = new Overlay();
   window.mScene = new Scene();
   window.mScroll = new Scroll(window.mScene);
   onScrollCommon(0);
 });
 
-const hideOverlay = (event) => {
-  document.getElementById('my-overlay').style.opacity = 0;
-  setTimeout(() => document.getElementById('my-overlay').style.display = 'none', 200);
-  window.addEventListener('wheel', onScrollDesktop, { passive: false });
-  window.addEventListener('touchmove', onScrollMobile, { passive: false });
-  window.removeEventListener('keyup', checkEscKey);
-};
-
-const showOverlay = (event) => {
-  document.getElementById('my-overlay').style.display = 'flex';
-  setTimeout(() => document.getElementById('my-overlay').style.opacity = 1, 100);
-  window.removeEventListener('wheel', onScrollDesktop);
-  window.removeEventListener('touchmove', onScrollMobile);
-  window.addEventListener('keyup', checkEscKey);
-};
-
-const checkEscKey = (event) => {
-  if (event.key && (event.key === 'Escape' || event.key === 'Esc')) {
-    hideOverlay();
-  }
-};
-
 const onScrollCommon = (deltaY) => {
   // TODO:
-  // scene.position.setY(Math.max(LAYERS_Y_OFFSET, scene.position.y + deltaY));
+  // scene.position.setY(Math.max(currentLayersOffsetY, scene.position.y + deltaY));
+
+  const scenePositionY = window.pageYOffset;
 
   const mShadowDiv = document.getElementById('my-shadow-div');
   const mInfoButton = document.getElementById('my-info-button');
 
-  // TODO:
-  // const shadowOpacity = 2 * (scene.position.y - LAYERS_Y_OFFSET) / window.innerHeight;
-  // const infoOpacity = 1.0 - (2 * (scene.position.y - LAYERS_Y_OFFSET) / window.innerHeight);
+  const shadowOpacity = 0.5 * (scenePositionY - currentLayersOffsetY) / window.innerHeight;
+  const infoOpacity = 1.0 - (2 * (scenePositionY - currentLayersOffsetY) / window.innerHeight);
 
-  // mShadowDiv.style.opacity = Math.max(0, Math.min(1, shadowOpacity));
-  // mInfoButton.style.opacity = Math.max(0, Math.min(1, infoOpacity));
-  // mInfoButton.style.display = (infoOpacity <= 0) ? 'none' : 'block';
+  mShadowDiv.style.opacity = Math.max(0, Math.min(1, shadowOpacity));
+  mInfoButton.style.opacity = Math.max(0, Math.min(1, infoOpacity));
+  mInfoButton.style.display = (infoOpacity <= 0) ? 'none' : 'block';
 
   // TODO:
   // window.mScroll.update(scene.position.y);
@@ -83,21 +58,22 @@ const onScrollCommon = (deltaY) => {
 };
 
 const onScrollDesktop = (event) => {
-  event.preventDefault();
+  // event.preventDefault();
   const deltaY = event.deltaY;
   onScrollCommon(deltaY);
 };
-// window.addEventListener('wheel', onScrollDesktop, { passive: false });
+window.addEventListener('wheel', onScrollDesktop, { passive: false });
 
-let touchDownY = null;
-window.addEventListener('touchstart', (event) => {
-  touchDownY = event.touches[0].clientY;
-});
-
-const onScrollMobile = (event) => {
-  event.preventDefault();
-  const deltaY = event.touches[0].clientY - touchDownY;
-  touchDownY = event.touches[0].clientY;
-  onScrollCommon(-deltaY);
-}
+// let touchDownY = null;
+// window.addEventListener('touchstart', (event) => {
+//   touchDownY = event.touches[0].clientY;
+// });
+// 
+// const onScrollMobile = (event) => {
+//   event.preventDefault();
+//   const deltaY = event.touches[0].clientY - touchDownY;
+//   touchDownY = event.touches[0].clientY;
+//   onScrollCommon(-deltaY);
+// }
 // window.addEventListener('touchmove', onScrollMobile, { passive: false });
+// Overlay.addEvent('touchmove', onScrollMobile);
